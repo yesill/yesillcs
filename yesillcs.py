5cd05789df27936f7aa4636043be3f527234aaad
yesill <yslmehmetbm@gmail.com>
2020-04-12 14:56:01 +0300

    yesillcs.py updated

--- yesillcs.py
+++ yesillcs.py
@@ -2,19 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("https://glacial-brook-22958.herokuapp.com/")
+@app.route("https://yesillcs.herokuapp.com/")
 def page_index():
     return render_template('index.html')
 
-@app.route("https://glacial-brook-22958.herokuapp.com/notifications")
+@app.route("https://yesillcs.herokuapp.com/notifications")
 def page_notifications():
     return render_template('notifications.html')
 
-@app.route("https://glacial-brook-22958.herokuapp.com/login")
+@app.route("https://yesillcs.herokuapp.com/login")
 def page_login():
     return render_template('login.html')
 
-@app.route("https://glacial-brook-22958.herokuapp.com/logs")
+@app.route("https://yesillcs.herokuapp.com/logs")
 def page_logs():
     return render_template('logs.html')
 
